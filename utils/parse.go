45d74382f66979e83cd8af2997c24035a25a3b4f
expiteRz <none@rz-public.xyz>
2023-08-08 17:14:46 +0900

    Wrong gamemode parse

--- utils/parse.go
+++ utils/parse.go
@@ -1,8 +1,6 @@
 package utils
 
 func CheckGameMode(ol int) (mode int) {
-	//fmt.Printf("%b\n", ol&0b11000110)
-
 	switch ol & 0b10000011000110 {
 	case 0b01000010:
 		mode = ModePrivateVS
@@ -12,11 +10,12 @@ func CheckGameMode(ol int) (mode int) {
 		mode = ModePrivateBalloonBattle
 	case 0b01000100:
 		mode = ModeVS
-	case 0b10000010000100:
+	case 0b10000100:
 		mode = ModeCoinBattle
+	case 0b10000010000100:
+		mode = ModeBalloonBattle
 	default:
 		mode = ModeNone
 	}
-
 	return
 }
