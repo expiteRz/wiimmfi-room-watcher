d4462276c1feb303ec16cc9d2723b9be0bcbdbd1
expiteRz <noreply@rz-public.xyz>
2024-04-29 21:04:39 +0900

    Refactor enumerations

--- utils/enum.go
+++ utils/enum.go
@@ -1,5 +1,15 @@
 package utils
 
+const (
+	ModeNone = iota - 1
+	ModeVS
+	ModeCoinBattle
+	ModeBalloonBattle
+	ModePrivateVS = iota - 1 + 0x10
+	ModePrivateCoinBattle
+	ModePrivateBalloonBattle
+)
+
 var (
 	ENGINE = []string{
 		"50cc",
@@ -16,16 +26,6 @@ var (
 		ModePrivateCoinBattle:    "Private Room Coin Battle",
 		ModePrivateBalloonBattle: "Private Room Balloon Battle",
 	}
-)
-
-const (
-	ModeNone = iota - 1
-	ModeVS
-	ModeCoinBattle
-	ModeBalloonBattle
-	ModePrivateVS = iota - 1 + 0x10
-	ModePrivateCoinBattle
-	ModePrivateBalloonBattle
 )
 
 // https://github.com/riidefi/mkw/blob/master/source/game/system/ResourceManager.hpp#L25
