5ce88ea64893601943138a1e5ed2760af82b7886
expiteRz <noreply@rz-public.xyz>
2024-07-05 12:03:35 +0900

    Add toggle to print debug log

--- utils/log/log.go
+++ utils/log/log.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"flag"
 	"github.com/rs/zerolog"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -10,20 +11,33 @@ import (
 	"time"
 )
 
+var debugToggle bool
 var Logger zerolog.Logger
 
 func init() {
+	flag.BoolVar(&debugToggle, "debug", false, "Toggle for debug mode to print the current state of room stat parsing")
+	flag.Parse()
+
 	Logger = zerolog.New(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
 		w.TimeFormat = time.DateTime
 		//w.FormatLevel = func(i interface{}) string {
 		//	return strings.ToUpper(fmt.Sprint("[", i, "]"))
 		//}
 		w.FormatCaller = func(i interface{}) string {
+			if i == nil {
+				return ""
+			}
 			if strings.Contains(filepath.Base(i.(string)), "main") {
 				return "[Main]"
 			}
 			dir := strings.Split(path.Dir(i.(string)), "/")
 			return "[" + cases.Title(language.AmericanEnglish).String(dir[len(dir)-1]) + "]"
 		}
-	})).Level(zerolog.DebugLevel).With().Timestamp().Caller().Logger()
+	}))
+	if debugToggle {
+		Logger = Logger.Level(zerolog.DebugLevel)
+	} else {
+		Logger = Logger.Level(zerolog.InfoLevel)
+	}
+	Logger = Logger.With().Timestamp().Caller().Logger()
 }
