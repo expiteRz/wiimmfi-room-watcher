a905b05058ef533554471787567b869d77e6acb5
expiteRz <noreply@rz-public.xyz>
2024-07-11 01:47:36 +0900

    Test commit Change game mode detection criteria (ol_status → race_mode) Refactor room joined log that have been accidentally turned into debug log

--- utils/parse.go
+++ utils/parse.go
@@ -19,3 +19,24 @@ func CheckGameMode(ol int) (mode int) {
 	}
 	return
 }
+
+func CheckGameModeTEST(raceMode int) int8 {
+	switch raceMode {
+	case 0x00:
+		return ModeNone
+	case 0x10:
+		return ModeVS
+	case 0x11:
+		return ModePrivateVS
+	case 0x30:
+		return ModeBalloonBattle
+	case 0x31:
+		return ModePrivateBalloonBattle
+	case 0x40:
+		return ModeCoinBattle
+	case 0x41:
+		return ModePrivateCoinBattle
+	default:
+		return ModeUnknown
+	}
+}
